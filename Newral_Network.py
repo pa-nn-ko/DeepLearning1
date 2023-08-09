02774876f607b57f3a17d83f787a7dca99d6fd56
pa-nn-ko <bboykit0404@gmail.com>
2023-08-10 01:58:27 +0900

    Newo

---
+++ Newral_Network.py
@@ -0,0 +1,9 @@
+#ステップ関数の定義と実行
+import numpy as np
+import matplotlib.pyplot as plt
+def step_function(x):
+    return np.where(x<=0,0,1)
+x = np.linspace(-5, 5)
+y = step_function(x)
+plt.plot(x,y)
+plt.show()
